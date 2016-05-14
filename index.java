5dfae4348d79c911738ef2847459aa9aa9a6b22a
Kayo Lucas <kayo_97@hotmail.com>
2016-05-14 17:12:37 -0300

    Criação da primeira Classe index
    sei nao

---
+++ index.java
@@ -0,0 +1,14 @@
+
+public class index {
+
+	
+	public index() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
