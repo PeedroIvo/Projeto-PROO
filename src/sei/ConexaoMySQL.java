11189de6be3b71ff940448381a7ea05a19a6b3fb
Pedro Ivo <pedroivo-hp@hotmail.com>
2016-06-17 05:15:25 -0300

    Só sei que nada sei... OMG

---
+++ src/sei/ConexaoMySQL.java
@@ -0,0 +1,31 @@
+package sei;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoMySQL implements IConexao {
+	static Connection conexao;
+	
+	@Override
+	public synchronized Connection getConexao(String baseDados, String enderecoBanco, String nomeBanco, String usuario,
+			String senha) {
+		conexao = null;
+		try {
+			conexao = DriverManager.getConnection(baseDados+"://"+enderecoBanco+"/"+ nomeBanco, usuario, senha);
+			System.out.println("Conectado!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conexao;
+	}
+	
+	public void fecharConexao() {
+		try {
+			ConexaoMySQL.conexao.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
